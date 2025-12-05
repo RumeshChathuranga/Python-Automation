fce7647df069f33fd2aeb7768ae9967222246ebb
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-05 22:04:32 +0530

    Getting focused news using the api

--- AccessAndBuild-APIs/News-API/main.py
+++ AccessAndBuild-APIs/News-API/main.py
@@ -1,6 +1,17 @@
 import requests
 
-r = requests.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=31010a085bfb4a27b46d1fca39605834')
-content = r.json()
-print(content['articles'][0]['title'])
-print(content['articles'][0]['description'])
+# r = requests.get('https://newsapi.org/v2/everything?q=united%20states&from=2025-12-04&to=2025-12-04&sortBy=popularity&apiKey=31010a085bfb4a27b46d1fca39605834')
+
+
+def get_news(topic,from_date,to_date,api_key):
+    url = f'https://newsapi.org/v2/everything?q={topic}&from={from_date}&to={to_date}&sortBy=popularity&apiKey={api_key}'
+    r = requests.get(url)
+    content = r.json() 
+    articles = content['articles']
+
+    results = []
+    for article in articles:
+        results.append(article['title'])
+    return results
+
+print(get_news('technology','2025-12-01','2025-12-04','31010a085bfb4a27b46d1fca39605834'))
